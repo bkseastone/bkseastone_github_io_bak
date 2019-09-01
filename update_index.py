34cc23542df608ffec39295c4bb0232372bf8a4e
weisongw <bkseastone@gmail.com>
2019-09-01 22:58:34 +0800

    thanks to my GF for adding the vue frame😘

--- update_index.py
+++ update_index.py
@@ -14,9 +14,9 @@ for i,line in enumerate(lines):
         LINE = i+1
         break
 
-prefix0 = '        <a href="./articles/html/'
+prefix0 = '      <div class="link-class"><a href="./articles/html/'
 prefix1 = '">'
-prefix2 = '</a><br>\n'
+prefix2 = '</a></div>\n'
 
 files_name = os.listdir('articles/html')
 files_html = []
@@ -24,7 +24,7 @@ for f_name in files_name:
     if os.path.splitext(f_name)[1]=='.html':
         files_html.append(f_name)
 for f_html in files_html:
-    lines.insert(LINE, prefix0+f_html+prefix1+f_html+prefix2)
+    lines.insert(LINE, prefix0+f_html+prefix1+f_html[:-5]+prefix2)
 
 s = ''.join(lines)
 fp = file('index.html', 'w')
